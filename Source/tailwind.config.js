00191801f4948bd3f14626ac516ffd75bad522db
Ian Yates <ian@snaptin.com>
2019-07-03 16:58:36 +0200

    Add files via upload

---
+++ Source/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '868px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '1920px',
+    },
+    fontFamily: {
+      display: ['Brandon Grotesque', 'sans-serif'],
+      body: ['PT Sans', 'serif'],
+    },
+    extend: {
+      colors: {
+        brand: '#1dd760',
+      }
+    }
+  }
+}
